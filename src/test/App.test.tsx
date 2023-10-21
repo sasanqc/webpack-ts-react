c1fc5d660e82dd0d9a6034170f18920b948c0044
sasan <sasanqc@gmail.com>
2023-10-21 17:57:47 +0330

    added jest and react testing library

---
+++ src/test/App.test.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react'
+import App from '../App'
+import '@testing-library/jest-dom'
+describe('App tests', () => {
+  it('should contains the heading 1', () => {})
+})
